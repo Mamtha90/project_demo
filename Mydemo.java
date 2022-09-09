bd7d1c7e8efc13b89ff2d3e6619f97e32bc65f49
mamth <mamthabai@gmail.com>
2022-09-09 09:02:00 +0000

    third commit of Mydemo.java

--- Mydemo.java
+++ Mydemo.java
@@ -1,6 +1,6 @@
 class Mydemo{
 
 	public static void main(String args[]){
-		System.out.println("Hello edureka, welcome to java world")
+		System.out.println("Hello edureka, welcome to java world again")
 	}
 }
